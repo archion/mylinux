a9e8dc3ea47e140f1284ca0f35be640ce4d6bb50
Zuodong Yu <richzyu@gmail.com>
2014-02-24 20:19:36 +0800

    backup

--- .oh-my-zsh/custom/my.zsh
+++ .oh-my-zsh/custom/my.zsh
@@ -1,12 +1,11 @@
 # 增强的历史补全
-ZSH_THEME="bira"
 bindkey '\e[A' history-beginning-search-backward
 bindkey '\e[B' history-beginning-search-forward
 bindkey '^U' backward-kill-line
 export EDITOR=vim
 export PATH=$PATH:~/bin:~/.cabal/bin
-export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=lcd'
 unalias gm
 alias ls='ls --time-style="+%Y-%m-%d %H:%M:%S"'
 alias ll='ls -lh'
 alias tmux="tmux -2"
+alias qmv="qmv -f do"
