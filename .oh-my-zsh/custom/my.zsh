4e5a8b1d353f7befb56c41e20ebbb1f6fcebcc58
Zuodong Yu <richzyu@gmail.com>
2015-07-07 18:23:58 +0800

    backup

--- .oh-my-zsh/custom/my.zsh
+++ .oh-my-zsh/custom/my.zsh
@@ -1,15 +1,15 @@
-# 增强的历史补全
-bindkey '\eOA' history-beginning-search-backward
-bindkey '\eOB' history-beginning-search-forward
-bindkey '^[[5~' beginning-of-history
-bindkey '^[[6~' end-of-history
-bindkey '^U' backward-kill-line
-export EDITOR=vim
-#add in /etc/profile
-#export PATH=$PATH:~/bin:~/.cabal/bin
-unalias gm
-alias ls='ls --time-style="+%Y-%m-%d %H:%M:%S"'
-alias ll='ls -lh'
-alias tmux="tmux -2"
-alias qmv="qmv -f do"
-alias fuck='$(thefuck $(fc -ln -1))'
+## 增强的历史补全
+#bindkey '\eOA' history-beginning-search-backward
+#bindkey '\eOB' history-beginning-search-forward
+#bindkey '^[[5~' beginning-of-history
+#bindkey '^[[6~' end-of-history
+#bindkey '^U' backward-kill-line
+#export EDITOR=vim
+##add in /etc/profile
+##export PATH=$PATH:~/bin:~/.cabal/bin
+#unalias gm
+#alias ls='ls --time-style="+%Y-%m-%d %H:%M:%S"'
+#alias ll='ls -lh'
+#alias tmux="tmux -2"
+#alias qmv="qmv -f do"
+#alias fuck='$(thefuck $(fc -ln -1))'
